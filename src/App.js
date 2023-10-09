5568362b5185d3a092e9d7286b3531bdfa5f6a94
davion-258 <hoangnam.itus@gmail.com>
2023-10-09 11:36:47 +0700

    Implement sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+function ProductCategoryRow({ category }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  const { name, price, stocked } = product;
+  const nameElement = stocked ? (
+    name
+  ) : (
+    <span style={{ color: "red" }}>{name}</span>
+  );
+  return (
+    <tr>
+      <td>{nameElement}</td>
+      <td>{price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products, filterText, stockedOnly }) {
+  const header = (
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+    </tr>
+  );
+  const rows = [];
+  let lastCat = null;
+  products.forEach((product) => {
+    const { category } = product;
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1 || (stockedOnly && !product.stocked)) {
+      return;
+    }
+    if (category !== lastCat) {
+      rows.push(
+        <ProductCategoryRow category={category} key={product.category} />
+      );
+      lastCat = category;
+    }
+
+    rows.push(<ProductRow product={product} key={product.name} />);
+  });
+  return (
+    <table>
+      <thead>{header}</thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({ filterText, stockedOnly, onFilterTextChange, onStockedOnlyChange }) {
+  return (
+    <form>
+      <div>
+        <input type="text" placeholder="Search..." value={filterText} onChange={event => onFilterTextChange(event.target.value)} />
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" checked={stockedOnly} onChange={event => onStockedOnlyChange(event.target.checked)} /> Only show products in
+          stock
+        </label>
+      </div>
+    </form>
+  );
+}
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [stockedOnly, setStockedOnly] = useState("");
+  return (
+    <div style={{ width: 250 }}>
+      <SearchBar filterText={filterText} stockedOnly={stockedOnly} onFilterTextChange={setFilterText} onStockedOnlyChange={setStockedOnly} />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        stockedOnly={stockedOnly}
+      />
     </div>
   );
 }
 
-export default App;
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
